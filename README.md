d131e9087d51d284c38654ca939b7af47280b988
Jeffrey <jc2573@cornell.edu>
2019-09-10 20:17:00 -0400

    Import jchou-web-v3

---
+++ README.md
@@ -0,0 +1,3 @@
+# jchou-web-v3
+
+Fourth exploration of my revamped personal website built from scratch with React, GraphQL, GraphCMS
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import ReactDom from 'react-dom';
+import { Root, Routes, addPrefetchExcludes } from 'react-static'
+import { Router, Match } from '@reach/router'
+import Dynamic from 'containers/Dynamic'
+import {Route} from 'react-router-dom';
+import {Header} from 'components/Header'
+import {Footer} from 'components/Footer'
+import { Helmet } from 'react-helmet'
+import './app.css'
+import { ScrollToTop } from "components/ScrollToTop"
+
+// Any routes that start with 'dynamic' will be treated as non-static routes
+addPrefetchExcludes(['dynamic'])
+
+function App() {
+  return (
+    <Root>
+      <Helmet>
+        <meta charSet="utf-8"></meta>
+        <title>Chou</title>
+        <link rel="icon" href="./assets/logo.png"></link>
+        <meta name="description" content="Hi, I’m a student at Cornell University studying Computer Science and Physics.
+        My interests include software/fullstack dev, and data science.
+        Built with React & GraphQL"></meta>
+      </Helmet>
+      <div className="content">
+        <React.Suspense fallback={
+        <div className="purple">
+          <div id="loading">Loading <div id="eyes">👀</div>
+          </div>
+          </div>
+          }>
+          <Router primary={false}>
+            <ScrollToTop path="/">
+              <Dynamic path="dynamic" />
+              <Routes path="*" />
+            </ScrollToTop>
+          </Router>
+        </React.Suspense>
+      </div>
+    </Root >
+  )
+}
+
+
+
+export default App
